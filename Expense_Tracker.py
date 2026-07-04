e38db275493e0af0549708d8ea5603758f4959f7
Abhijit Sarma <abhijitsarma.edu@gmail.com>
2026-07-04 21:24:28 +0530

    Improved input validation and simplified expense calculations

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -1,102 +1,80 @@
-# Expense Tracker
+# Goals to acheive : [1] Stop Repeated Coding [2] Get Containers in a Dictionary [3] Try/Except for error handling
 
-# Empty Containers based on basic categories
+# Containers | Needed to be in a single Dictionary | Can't Get it done for the time
 food = []
 shopping = []
 bills = []
 miscellaneous = []
 
 print()
-# Menu
+# Menu Board
 print('-----MENU-----')
 print()
 print('A. Food')
 print('B. Shopping')
 print('C. Bills')
 print('D. Miscellaneous')
-print('Q -----> Quit')
-print()
-
-# Entry of expenses to the lists 
-while True:
-    menu_entry = input('Expense Category: ')
-    if menu_entry.capitalize() == 'Q':
-        break
-    elif menu_entry.capitalize() == 'A':
-        expense_entry_A = int(input('Enter the Amount: '))
-        food.append(expense_entry_A)
-        print(f'{expense_entry_A} has been added to Food.')
-    elif menu_entry.capitalize() == 'B':
-        expense_entry_B = int(input('Enter the Amount: '))
-        shopping.append(expense_entry_B)
-        print(f'{expense_entry_B} has been added to Shopping.')
-    elif menu_entry.capitalize() == 'C':
-        expense_entry_C = int(input('Enter the Amount: '))
-        bills.append(expense_entry_C)
-        print(f'{expense_entry_C} has been added to Bills.')
-    elif menu_entry.capitalize() == 'D':
-        expense_entry_D = int(input('Enter the Amount: '))
-        miscellaneous.append(expense_entry_D)
-        print(f'{expense_entry_D} has been added to miscellaneous.')
-print()
-print()
-
-# OPERATIONS FUNCTIONS ----- LISTS 
-
-# Adding of expenses of single lists 
-def food_expenses ():
-    total_food = sum(food)
-    print(f'Your Total Food Expense is ${total_food}')
-
-def shopping_expenses ():
-    total_shopping = sum(shopping)
-    print(f'Your Total Shopping Expense is ${total_shopping}')
-
-def bills_expenses ():
-    total_bills = sum(bills)
-    print(f'Your Total Bills Expense is ${total_bills}')
-
-def miscellaneous_expenses ():
-    total_miscellaneous = sum(miscellaneous)
-    print(f'Your Total Miscellaneous Expense is ${total_miscellaneous}')
-
-# Adding of all the expenses 
-def full_expense ():
-    total_expense = (
-        sum(food)
-        + sum(shopping)
-        + sum(bills)
-        + sum(miscellaneous)
-    )
-    print(f'Your Overall Expense is ${total_expense}')
-
-# USER SIDED LIST OPS
-# OPS MENU
-print('-----OPERATIONS MENU------')
-print()
-print('A. Total Food Expense')
-print('B. Total Shopping Expense')
-print('C. Total Bills Expense')
-print('D. Total Miscellaneous Expense')
-print('E. Overall Expenses')
 print('Q. Quit')
 print()
 
-# OPS LOGIC
-while True:
-    ops_entry = input('VIEW EXPENSES: ')
-    if ops_entry.capitalize() == "Q":
-        break
-    elif ops_entry.capitalize() == 'A':
-        food_expenses()
-    elif ops_entry.capitalize() == 'B':
-        shopping_expenses()
-    elif ops_entry.capitalize() == 'C':
-        bills_expenses()
-    elif ops_entry.capitalize() == 'D':
-        miscellaneous_expenses()
-    elif ops_entry.capitalize() == 'E':
-        full_expense()
-    else:
-        print('Invalid Option')
 
+# User Input of the Menu board | Added: Error Handling | Problem: Error in last category leads to input redo of all category.
+while True:
+    menu_entry = input('Expense Category: ').upper().strip()
+# Items Selection Error Handling
+    if menu_entry == 'Q':
+        break
+    if menu_entry not in ['A','B','C','D']:
+        print('Invalid Category, Please Re-enter')
+        continue
+# Items Expense Entry Error Handling    
+    try:
+        expense_entry = int(input('Enter Expense Amount: '))
+    except ValueError:
+        print('Invalid Expense, Please Re-enter')
+        continue
+# User Input Operations
+    if menu_entry == 'A':
+        food.append(expense_entry)
+    elif menu_entry == 'B':
+        shopping.append(expense_entry)
+    elif menu_entry == 'C':
+        bills.append(expense_entry)
+    elif menu_entry == 'D':
+        miscellaneous.append(expense_entry)
+
+# 2nd Menu
+print()
+print('-----View Expense Menu------')
+print()
+print('A. View Single Category Expense')
+print('B. View Overall Expense')
+print('Q. Quit')
+print()
+
+# Defining Functions
+def Overall_Expenses ():
+    overall_expense = sum(food + shopping + bills + miscellaneous)
+    print(f'Your Overall Expense: ${overall_expense}')
+    print()
+
+def Single_Category_Expense():
+    expenses = [sum(food),sum(shopping),sum(bills),sum(miscellaneous)]
+    print(f'Food Expenses: ${expenses[0]}')
+    print(f'Shopping Expenses: ${expenses[1]}')
+    print(f'Bills Expenses: ${expenses[2]}')
+    print(f'Miscellaneous Expense: ${expenses[3]}')
+    print()
+
+#  Menu Input Logic
+while True:
+    menu_2 = input('View Expense:' ).upper().strip()
+    if menu_2 == 'Q':
+        break
+    if menu_2 not in ['A','B']:
+        print('Invalid Input')
+        continue
+    if menu_2 == 'A':
+        Single_Category_Expense()
+    elif menu_2 == 'B':
+        Overall_Expenses()
